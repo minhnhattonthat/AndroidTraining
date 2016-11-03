d7a6d94a54dcb16373c2cc5a3accc4ab197c5a6a
Nhat Ton <minhnhattonthat@gmail.com>
2016-11-03 20:11:45 +0700

    bai tap linear layout weight

---
+++ BioApplication/app/src/main/java/com/example/bioapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.bioapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private static final String[] genderList = new String[]{
+            "Male", "Female", "N/A"
+    };
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line,
+                genderList);
+        AutoCompleteTextView genderTextView = (AutoCompleteTextView)
+                findViewById(R.id.gender);
+        genderTextView.setAdapter(genderAdapter);
+
+        EditText yobTextView = (EditText)
+                findViewById(R.id.yob);
+        yobTextView.setFilters();
+    }
+
+
+}
