2a0919840c2e7c3a130fa1fc623dcba5546ca540
Nhat Ton <minhnhattonthat@gmail.com>
2016-11-12 18:22:54 +0700

    applying efficient adapter

--- Notepadv3/app/src/main/java/com/android/demo/notepad3/Notepadv3.java
+++ Notepadv3/app/src/main/java/com/android/demo/notepad3/Notepadv3.java
@@ -17,25 +17,30 @@
 package com.android.demo.notepad3;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 
 public class Notepadv3 extends ListActivity {
     public static final DateFormat df = DateFormat.getDateTimeInstance();
-    public static String titleUnedited;
-    public static String bodyUnedited;
+    public static String titleUnedited = "";
+    public static String bodyUnedited = "";
 
     private static final int ACTIVITY_CREATE = 0;
     private static final int ACTIVITY_EDIT = 1;
@@ -45,36 +50,97 @@ public class Notepadv3 extends ListActivity {
     private static final int DUPLICATE_ID = Menu.FIRST + 2;
 
     private NotesDbAdapter mDbHelper;
+    private Cursor mNotesCursor;
+
+    private class EfficientAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+
+        public Cursor mCursor;
+
+        public String[] mFrom;
+        public int[] mTo;
+
+        public EfficientAdapter(Context context, Cursor cursor) {
+            mCursor = cursor;
+
+            mInflater = LayoutInflater.from(context);
+        }
+
+        public int getCount() {
+            return mCursor.getCount();
+        }
+
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+
+        public long getItemId(int position) {
+            mCursor.moveToPosition(position);
+            return mCursor.getLong(mCursor.getColumnIndexOrThrow(NotesDbAdapter.KEY_ROWID));
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.notes_row, null);
+
+                holder = new ViewHolder();
+                holder.text1 = (TextView) convertView.findViewById(R.id.text1);
+                holder.text2 = (TextView) convertView.findViewById(R.id.text2);
+                holder.text3 = (TextView) convertView.findViewById(R.id.text3);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.text1.setText(mCursor.getString(
+                    mCursor.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE)));
+            holder.text2.setText(mCursor.getString(
+                    mCursor.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
+            holder.text3.setText(mCursor.getString(
+                    mCursor.getColumnIndexOrThrow(NotesDbAdapter.KEY_DATE)));
+
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView text1;
+            TextView text2;
+            TextView text3;
+        }
+    }
 
     /**
      * Called when the activity is first created.
      */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.notes_list);
+
         mDbHelper = new NotesDbAdapter(this);
         mDbHelper.open();
+
         fillData();
+
         registerForContextMenu(getListView());
     }
 
     private void fillData() {
-        // Get all of the rows from the database and create the item list
-        Cursor mNotesCursor = mDbHelper.fetchAllNotes();
+        mNotesCursor = mDbHelper.fetchAllNotes();
         startManagingCursor(mNotesCursor);
+        if (mNotesCursor != null) {
+            EfficientAdapter notes = new EfficientAdapter(this, mNotesCursor);
 
-        // Create an array to specify the fields we want to display in the list (only TITLE)
-        String[] from = new String[]{NotesDbAdapter.KEY_TITLE, NotesDbAdapter.KEY_BODY, NotesDbAdapter.KEY_DATE};
-
-        // and an array of the fields we want to bind those fields to (in this case just text1)
-        int[] to = new int[]{R.id.text1, R.id.text2, R.id.text3};
-
-        // Now create a simple cursor adapter and set it to display
-        SimpleCursorAdapter notes =
-                new SimpleCursorAdapter(this, R.layout.notes_row, mNotesCursor, from, to);
-
-        setListAdapter(notes);
+            setListAdapter(notes);
+        }
     }
 
     @Override
@@ -132,7 +198,8 @@ public class Notepadv3 extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-        Cursor note = mDbHelper.fetchNote(id);
+        Cursor note = mNotesCursor;
+        note.moveToPosition(position);
         titleUnedited = note.getString(
                 note.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE));
         bodyUnedited = note.getString(
